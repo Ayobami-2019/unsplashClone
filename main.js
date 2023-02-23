a0c73d7ddaa26e079c6e8a18c0d9500c45717cf9
Dunmoye Ayobami <ayobamidunmoye2000@gmail.com>
2023-02-23 17:50:01 +0100

    header edit

---
+++ main.js
@@ -0,0 +1,7 @@
+const next=document.querySelector('.next');
+const animate=document.querySelector('.animate')
+
+next.addEventListener("click", (e) => {
+    // if (hamburgerDiv.style.display === "none") {
+      animate.scroll=screenLeft
+})
